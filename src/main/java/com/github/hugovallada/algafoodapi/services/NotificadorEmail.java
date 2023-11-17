5a00f65af4bc4272f7515e2a8376406321ac422d
Hugo <valladahugo@gmail.com>
2023-11-16 22:10:11 -0300

    Criando propriedades customizadas com @Value

--- src/main/java/com/github/hugovallada/algafoodapi/services/NotificadorEmail.java
+++ src/main/java/com/github/hugovallada/algafoodapi/services/NotificadorEmail.java
@@ -3,6 +3,7 @@ package com.github.hugovallada.algafoodapi.services;
 import com.github.hugovallada.algafoodapi.models.Cliente;
 import com.github.hugovallada.algafoodapi.ports.Notificador;
 import com.github.hugovallada.algafoodapi.util.annotations.TipoDoNotificador;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.github.hugovallada.algafoodapi.util.annotations.helpers.NivelUrgencia.URGENTE;
@@ -11,10 +12,15 @@ import static com.github.hugovallada.algafoodapi.util.annotations.helpers.NivelU
 @TipoDoNotificador(URGENTE)
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor")
+    private String host;
+    @Value("notificador.email.port-servidor")
+    private int port;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do email %s: %s%n",
-                cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("Notificando %s através do email %s, no host %s:%d: %s%n",
+                cliente.getNome(), cliente.getEmail(), host, port, mensagem);
     }
 
 }
